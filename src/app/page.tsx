a0eb47002b6caf92e0cd8a685a9be2afe61f7716
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-10 20:21:06 +0900

    色々やったのをとりあえず保存

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+'use client';
 import Image from 'next/image';
 import { Avatar } from '@mui/material';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-4xl">Welcome to the toybox!</h1>
-      <p className="text-lg mt-4">
-        This is a portfolio site of suguru-toyohara.</p>
+      <p className="text-lg m-4"> This is a portfolio site of suguru-toyohara.</p>
       <Avatar sx={{ width: 100, height: 100, bgcolor: 'black', border: "1px solid #AAA"}}><Image src="/icon.svg" alt="icon" width={100} height={100}/></Avatar>
+      <h2 className="text-2xl m-8">GitHub Contributions</h2>
+      
     </main>
   );
 }
---
+++ src/components/GithubActivityCalendar.tsx
@@ -0,0 +1,10 @@
+import ActivityCalendar, { Activity } from "react-activity-calendar";
+
+export function GithubActivityCalendar (props: {data: Activity[]}) {
+  
+  return (
+    <>
+      <ActivityCalendar colorScheme="dark" data={props.data}/>
+    </>
+  );
+};

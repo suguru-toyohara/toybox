dd010bda2826e8a0d41a07d5bff32e0e9ee3c58d
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-10 21:27:31 +0900

    Social Linkを作成(github)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,32 @@
 'use client';
 import Image from 'next/image';
-import { Avatar } from '@mui/material';
+import { useRouter } from 'next/navigation';
+import { Avatar, Box, Button, ButtonBase, Grid } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+const openGitHub = () => {
+};
 
 export default function Home() {
+  const router = useRouter();
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-w-max flex-col items-center p-24">
       <h1 className="text-4xl">Welcome to the toybox!</h1>
-      <p className="text-lg m-4"> This is a portfolio site of suguru-toyohara.</p>
-      <Avatar sx={{ width: 100, height: 100, bgcolor: 'black', border: "1px solid #AAA"}}><Image src="/icon.svg" alt="icon" width={100} height={100}/></Avatar>
-      <h2 className="text-2xl m-8">GitHub Contributions</h2>
-      
+      <p className="text-lg m-4"> This is a portfolio site of suguru-toyohara(simeji).</p>
+      <Avatar sx={{ width: 100, height: 100, bgcolor: 'black', border: "1px solid #AAA"}} className='m-8'>
+        <Image src="/icon.svg" alt="icon" width={100} height={100}/>
+      </Avatar>
+
+      {/* <h2 className="text-2xl m-8">GitHub Contributions</h2> */}
+      <h2 className="text-2xl m-8">Social Links</h2>
+      <a href="https://github.com" target='_blank'>
+        <ButtonBase >
+          <Box className="p-4">
+            <GitHubIcon sx={{ fontSize: "24px" }}/>
+            <span className="text-lg m-4"> suguru-toyohara </span>
+          </Box>
+        </ButtonBase>
+      </a>
     </main>
   );
 }

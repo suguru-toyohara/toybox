4520e11e0c088ae8a00ea05dd1e564e6ab473bcd
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-10 19:09:01 +0900

    サーバルちゃんiconを導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to the toybox!</h1>
-      <p className="text-lg"> Hello, World </p>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-4xl">Welcome to the toybox!</h1>
+      <p className="text-lg mt-4">
+        This is a portfolio site of suguru-toyohara.</p>
+      <Image src="/icon.svg" alt="icon" width={200} height={200} />
     </main>
   );
 }

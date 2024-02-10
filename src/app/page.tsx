8f61074819c5f59d3237dfac6b85f9c74f7f83cf
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-10 19:25:51 +0900

    avatarを導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image';
-
+import { Avatar } from '@mui/material';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-4xl">Welcome to the toybox!</h1>
       <p className="text-lg mt-4">
         This is a portfolio site of suguru-toyohara.</p>
-      <Image src="/icon.svg" alt="icon" width={200} height={200} />
+      <Avatar sx={{ width: 100, height: 100, bgcolor: 'black', border: "1px solid #AAA"}}><Image src="/icon.svg" alt="icon" width={100} height={100}/></Avatar>
     </main>
   );
 }

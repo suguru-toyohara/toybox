46745e89e76b91c51a4ae352928acd6547deb340
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-11 19:59:56 +0900

    build failed修正

--- src/app/api/v1/github/route.ts
+++ src/app/api/v1/github/route.ts
@@ -1,5 +1,6 @@
 import { Activity } from "@/types/calendar";
 
+export const runtime = 'edge';
 // async function GithubActivitiesHandler({username}: {username: string}): Promise<Response> {
 //   const url = `https://github.com/users/${username}/contributions`;
 

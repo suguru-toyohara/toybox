6671dc1b5f3eb6b50c5670eaf2e7e1c3194381f4
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-11 10:48:02 +0900

    Tabコンポーネントを光らせるよう変更

--- src/components/header/HoverButton.tsx
+++ src/components/header/HoverButton.tsx
@@ -5,7 +5,6 @@ import { useRouter } from 'next/navigation';
 export default function HoverButton( {text, href, Icon} : {text: string, href: string, Icon?: React.ElementType} ){
   const router = useRouter();
   return (
-    // hrefをonclickで実装する
     <Button className="text-2xl" sx={{ "&:hover" : { backgroundColor: "#222" }}} onClick={() => router.push(href)}>
       {Icon && <Icon className="text-2xl text-white" />}
       <Typography sx={{ textTransform: 'capitalize', color: "#FFF" }}>
--- src/components/header/Tab.tsx
+++ src/components/header/Tab.tsx
@@ -1,10 +1,17 @@
+'use client'
 import { Box } from "@mui/material";
+import { usePathname } from "next/navigation";
 import HoverButton from "./HoverButton";
 
 export default function Tab({title, href, Icon} : {title: string, href: string, Icon?: React.ElementType}){
+  const pathname = usePathname();
+  const highlight = {margin: "0px 10px", borderBottom: "3px solid #ff4d00"};
+  const normal = {margin: "0px 10px"};
+  const boxStyle = href === pathname ? highlight : normal;
+
   return (
     // TODO: 光らせるプロパティを追加する
-    <Box sx={{ padding : "10px" }}>
+    <Box sx={boxStyle}>
       <HoverButton text={title} Icon={Icon} href={href} />
     </Box>
   );

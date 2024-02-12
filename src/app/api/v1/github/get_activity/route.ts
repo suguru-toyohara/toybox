e0c74b081c2b5df0764ce88f6d71de81dc45fd8c
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-12 10:21:08 +0900

    環境変数を変更

--- src/app/api/v1/github/get_activity/route.ts
+++ src/app/api/v1/github/get_activity/route.ts
@@ -4,7 +4,7 @@ export const runtime = 'edge';
 
 
 async function handler() {
-  const url = process.env.URL + `/username/suguru-toyohara`;
+  const url = process.env.GITHUB_API_URL + `/username/suguru-toyohara`;
   const response = await fetch(url);
   const activities: Activity[] = await response.json();
   return NextResponse.json(activities);
